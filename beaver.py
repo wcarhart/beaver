0affa27db8c88fad747e24af7f79e59b8178515e
Will Carhart <wcarhart@sandiego.edu>
2019-04-06 21:01:45 -0700

    Starting dev testing of range parsing

--- beaver.py
+++ beaver.py
@@ -18,91 +18,22 @@ def parse_range(string):
 		:string: (str) desired range of logging
 	"""
 	if not ' to ' in string:
-		errprint('Invalid format for range')
+		errprint("Invalid format for range")
 		sys.exit(1)
 
 	start, end = string.split(' to ')[0], string.split(' to ')[1]
-	if not is_date(start, fuzzy=False):
-		errprint('Invalid format for start of range')
-		sys.exit(1)
-	if not is_date(end, fuzzy=False):
-		errprint('Invalid format for end of range')
-		sys.exit(1)
+
+	start_datetime = validate_datetime(start)
+	end_datetime = validate_datetime(end)
+	validate_range(start_datetime, end_datetime)
+
 	return start, end
 
-def validate_datetime(string):
+def validate_date(date):
 	"""
-	Validate datetime given in range
-
-	Follows these steps:
-		1. Parse by keywords (on, at), or check if contains date and/or time
-		3. Parse time
-		4. Parse date
+	Validate string as a date format
+		:date: (str) the date to validate
 	"""
-	time = date = ''
-
-	# 1. Parse by keywords (on, at)
-	if ' on ' in string:
-		# `time` on `date`
-		time, date = string.split(' on ')[0], string.split(' on ')[1]
-	elif ' at ' in string:
-		# `date` at `time`
-		time, date = string.split(' at ')[1], string.split(' at ')[0]
-	else:
-		if ':' in string:
-			# contains time
-			time_index = [index for index, s in enumerate(string.split()) if ':' in s][0]
-			time = string[time_index:]
-			date = string[:time_index]
-		elif len(string.split()) == 1 and ('am' in string.lower() or 'pm' in string.lower()):
-			# contains only time
-			time = string
-		else:
-			# does not contain time
-			date = string
-
-	time = time.strip()
-	date = date.strip()
-	time = ''.join([t.lower() for t in time])
-	date = ''.join([d.lower() for d in date])
-
-	# 3. Parse time
-	if time == '':
-		hours = '0'
-		minutes = '0'
-		seconds = '0'
-	else:
-		if ':' in time:
-			hours, minutes, seconds = tuple(time.split(':') if len(time.split(':')) == 3 else time.split(':')+['0'])
-		else:
-			hours = time
-			minutes = seconds = '0'
-
-		if 'am' in hours or 'am' in minutes or 'am' in seconds:
-			is_am = True
-			is_24 = False
-			hours = hours.replace('am', '')
-			minutes = minutes.replace('am', '')
-			seconds = seconds.replace('am', '')
-		elif 'pm' in hours or 'pm' in minutes or 'pm' in seconds:
-			is_am = False
-			is_24 = False
-			hours = hours.replace('pm', '')
-			minutes = minutes.replace('pm', '')
-			seconds = seconds.replace('pm', '')
-		else:
-			is_24 = True
-
-		if not is_24:
-			if not is_am:
-				hours = str(int(hours) + 12)
-
-	if not hours.isdigit() or not minutes.isdigit() or not seconds.isdigit():
-		print(hours, minutes, seconds)
-		errprint("Could not determine time format")
-		sys.exit(1)
-
-	# 4. Parse date
 	days = {
 		'monday': 0,
 		'tuesday': 1,
@@ -210,31 +141,117 @@ def validate_datetime(string):
 	month = new_date.month
 	year = new_date.year
 
-	print(f"Hours: {hours}")
-	print(f"Minutes: {minutes}")
-	print(f"Seconds: {seconds}")
+	return year, month, day
 
-	print(f"Day: {day}")
-	print(f"Month: {month}")
-	print(f"Year: {year}")
-
-def normalize_datetime(dt):
-	# TODO
-	return
-
-def is_date(date, fuzzy=False):
+def validate_time(time):
 	"""
-	Return whether the string can be interpreted as a date
-		:string: (str) string to check for date
-		:fuzzy: (bool) ignore unknown tokens in string if True
+	Validate string as a time format
+		:time: (str) the time to validate
 	"""
-	try:
-		dateutil.parser.parse(date, fuzzy=fuzzy)
-		return True
-	except ValueError:
-		if date.lower() in ['today', 'yesterday']:
-			return True
-		return False
+
+	if time == '':
+		hours = '0'
+		minutes = '0'
+		seconds = '0'
+	else:
+		if ':' in time:
+			hours, minutes, seconds = tuple(time.split(':') if len(time.split(':')) == 3 else time.split(':')+['0'])
+		else:
+			hours = time
+			minutes = seconds = '0'
+
+		if 'am' in hours or 'am' in minutes or 'am' in seconds:
+			is_am = True
+			is_24 = False
+			hours = hours.replace('am', '')
+			minutes = minutes.replace('am', '')
+			seconds = seconds.replace('am', '')
+		elif 'pm' in hours or 'pm' in minutes or 'pm' in seconds:
+			is_am = False
+			is_24 = False
+			hours = hours.replace('pm', '')
+			minutes = minutes.replace('pm', '')
+			seconds = seconds.replace('pm', '')
+		else:
+			is_24 = True
+
+		if not is_24:
+			if not is_am:
+				hours = str(int(hours) + 12)
+
+	if not hours.isdigit() or not minutes.isdigit() or not seconds.isdigit():
+		print(hours, minutes, seconds)
+		errprint("Could not determine time format")
+		sys.exit(1)
+
+	return int(hours), int(minutes), int(seconds)
+
+def validate_datetime(string):
+	"""
+	Validate datetime given in range
+
+	Follows these steps:
+		0. Shortcuts
+		1. Parse by keywords (on, at), or check if contains date and/or time
+		2. Parse time
+		3. Parse date
+	"""
+	# Shortcuts
+	shortcuts = ['today', 'yesterday', 'now']
+	if string in shortcuts:
+		if string == 'today' or string == 'now':
+			return datetime.datetime.today()
+		elif string == 'yesterday':
+			return datetime.datetime.now() - datetime.timedelta(days=1)
+
+	time = date = ''
+
+	# Parse by keywords (on, at)
+	if ' on ' in string:
+		# `time` on `date`
+		time, date = string.split(' on ')[0], string.split(' on ')[1]
+	elif ' at ' in string:
+		# `date` at `time`
+		time, date = string.split(' at ')[1], string.split(' at ')[0]
+	else:
+		if ':' in string:
+			# contains time
+			time_index = [index for index, s in enumerate(string.split()) if ':' in s][0]
+			time = string[time_index:]
+			date = string[:time_index]
+		elif len(string.split()) == 1 and ('am' in string.lower() or 'pm' in string.lower()):
+			# contains only time
+			time = string
+		else:
+			# does not contain time
+			date = string
+
+	time = time.strip()
+	date = date.strip()
+	time = ''.join([t.lower() for t in time])
+	date = ''.join([d.lower() for d in date])
+
+	# Parse time
+	hours, minutes, seconds = validate_time(time)
+
+	# Parse date
+	year, month, day = validate_date(date)
+
+	return datetime.datetime(year=year, month=month, day=day, hour=hours, minute=minutes, second=seconds)
+
+def validate_range(start, end):
+	"""
+	Validate the range of two datetimes
+		:start: (datetime) the start of the range
+		:end: (datetime) the end of the range
+	"""
+	if end > start:
+		errprint("End of range occurs before start of range")
+		sys.exit(1)
+
+	if end > datetime.datetime.now()
+		errprint("End of range hasn't happened yet")
+		sys.exit(1)
 
 def acquire_log_files():
 	return get_by_filetype(['log', 'logs'])
